4f76d655c3876d3357bc9f15f7fa760d3880b128
Hrenaki <shikhovwtr@mail.ru>
2021-02-20 17:03:57 +0700

    Добавьте файлы проекта.

---
+++ CG_SplineVisualizer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CG_SplineVisualizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (new Window(800, 600, "Window")).Run();
+        }
+    }
+}
---
+++ SplineTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SplineTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SplineTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0e1d59ad-88f5-45eb-a68e-3d3a803327a4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
